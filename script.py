277a84297614ee27b9961f5de2b932a2938037bc
Digital Druid <playerskingdom@hotmail.com>
2024-11-04 18:36:57 -0800

    Created Lobby and 2 games added in comments for ref

--- script.py
+++ script.py
@@ -1,6 +1,34 @@
 class Lobby:
     def __init__(self):
-        self.game("Blackjack", "Roulette")
+        self.games = ["Blackjack", "Roulette"]  # List of games
         self.current_game = None
-        self.player_balance = 1000 #starting balance
-        
+        self.player_balance = 1000  # Starting balance
+
+
+class Blackjack:
+    def __init__(self):
+        self.players = []  # Area for the players
+        self.deck = self.create_deck()  # Method to create a triple deck of cards
+        self.dealer = None  # Dealer
+        self.bet = 0  # Player's bet amount
+
+    def create_deck(self):
+        # Logic to create a triple deck of cards
+        pass
+
+
+class Roulette:
+    def __init__(self):
+        self.players = []  # Area for the players
+        self.wheel = self.create_wheel()  # Method to set up the roulette wheel
+        self.dealer = None  # Dealer
+        self.table = {}  # Dictionary to place bets
+
+    def create_wheel(self):
+        # Logic to create an American roulette wheel
+        pass
+
+
+
+
+
